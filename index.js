35aec4698a781ba165de9396674f6653e1d32c97
Abe Rudy <aberudy@gmail.com>
2017-11-18 15:01:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code in this file!
+const currentUser = "Abe"
+
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
+
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!"
+
+let shortGreeting = "Welcome, " + currentUser.charAt(0) + "!"
